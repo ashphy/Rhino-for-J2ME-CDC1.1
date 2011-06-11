92e6ba8ab1153513166661566473328ad298adf6
kazuki-h <kazuki-h@is.naist.jp>
2011-06-11 22:03:43 +0900

    Add javadoc of PropertyChange

--- src/jp/naist/ricoh2011jp/mandara/javabeans/PropertyChangeEvent.java
+++ src/jp/naist/ricoh2011jp/mandara/javabeans/PropertyChangeEvent.java
@@ -2,6 +2,14 @@ package jp.naist.ricoh2011jp.mandara.javabeans;
 
 import java.util.EventObject;
 
+/**
+ * A "PropertyChange" event gets delivered whenever a bean changes a "bound" or "constrained" property. A PropertyChangeEvent object is sent as an argument to the PropertyChangeListener and VetoableChangeListener methods.
+ * Normally PropertyChangeEvents are accompanied by the name and the old and new value of the changed property. If the new value is a primitive type (such as int or boolean) it must be wrapped as the corresponding java.lang.* Object type (such as Integer or Boolean).
+ * Null values may be provided for the old and the new values if their true values are not known.
+ * An event source may send a null object as the name to indicate that an arbitrary set of if its properties have changed. In this case the old and new values should also be null.
+ * 
+ * @author Kazuki-h
+ */
 public class PropertyChangeEvent extends EventObject {
 
 	private static final long serialVersionUID = 295203419106580795L;
@@ -11,6 +19,13 @@ public class PropertyChangeEvent extends EventObject {
     Object newValue;
     Object propagationId;
 
+    /**
+     * Constructs a new PropertyChangeEvent.
+     * @param source
+     * @param propertyName
+     * @param oldValue
+     * @param newValue
+     */
     public PropertyChangeEvent(Object source, String propertyName, Object oldValue, Object newValue) {
         super(source);
 
@@ -19,23 +34,44 @@ public class PropertyChangeEvent extends EventObject {
         this.newValue = newValue;
     }
 
+    /**
+     * Sets the new value for the property, expressed as an Object.
+     * @return
+     */
+    public Object getNewValue() {
+        return newValue;
+    }
+    
+
+    /**
+     * Gets the old value for the property, expressed as an Object.
+     * @return
+     */
+    public Object getOldValue() {
+        return oldValue;
+    }
+    
+    /**
+     * The "propagationId" field is reserved for future use.
+     * @return
+     */
+    public Object getPropagationId() {
+        return propagationId;
+    }
+    
+    /**
+     * Gets the programmatic name of the property that was changed.
+     * @return
+     */
     public String getPropertyName() {
         return propertyName;
     }
 
+    /**
+     * Sets the propagationId object for the event.
+     * @param propagationId
+     */
     public void setPropagationId(Object propagationId) {
         this.propagationId = propagationId;
-    }
-
-    public Object getPropagationId() {
-        return propagationId;
-    }
-
-    public Object getOldValue() {
-        return oldValue;
-    }
-
-    public Object getNewValue() {
-        return newValue;
     }
 }
--- src/jp/naist/ricoh2011jp/mandara/javabeans/PropertyChangeListener.java
+++ src/jp/naist/ricoh2011jp/mandara/javabeans/PropertyChangeListener.java
@@ -1,5 +1,15 @@
 package jp.naist.ricoh2011jp.mandara.javabeans;
 
+/**
+ * A "PropertyChange" event gets fired whenever a bean changes a "bound" property. 
+ * You can register a PropertyChangeListener with a source bean so as to be notified of any bound property updates.
+ * @author Kazuki-h
+ */
 public interface PropertyChangeListener {
+	
+	/**
+	 * This method gets called when a bound property is changed.
+	 * @param evt
+	 */
 	void propertyChange(PropertyChangeEvent evt);
 }
